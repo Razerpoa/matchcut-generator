9cfe02177d42aaa295af856579dacca96484757e
Razerpoa <fathandwipayana@gmail.com>
2026-01-28 17:33:50 +0700

    Remove unused imports.

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from PIL import Image
 from undetected_chromedriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
